7d572956c303b31a71e729cb27a3601e2703eb1e
Yiningww <wangyining0609@gmail.com>
2025-02-16 00:39:07 -0500

    added a function that returns a flow index to dependency order dict

--- New_setting/new_setting.py
+++ New_setting/new_setting.py
@@ -1,3 +1,7 @@
+"""
+New flow information setting, replacement of generic.py
+"""
+
 import os
 import json
 import argparse
@@ -6,47 +10,10 @@ from typing import List
 from collections import defaultdict, deque
 import numpy as np
 
-# TO DO: add a dict that store each flow's order
-
-
-def dfs(current_flow, visited, graph, order):
-    if current_flow in visited:
-        return
-    visited.add(current_flow)
-    for depenency in graph[current_flow]:
-        dfs(depenency, visited, graph, order)
-    order.append(current_flow)
-    return order
-
-def get_dependency_order(flow_info):
-    """
-    key: (collective, group) value: flows
-    """
-
-    groups = defaultdict(dict)
-    for flow_id, flow in flow_info.items():
-        groups[(flow["collective"], flow["group"])][flow_id] = flow
-
-    dependency_orders = {}
-    for group_key, flows in groups.items(): # e.g. (k=1, n=2), {'1': {'collective': 1, 'group': 1, 'source': 1, 'dest': 10, 'data_size': 150.0, 'links': {1, 2, 3, 4}, 'dependency_order': []}})
-        # Construct graph
-        graph = {flow_id: [] for flow_id in flows}
-        for flow_id, flow in flows.items():
-            for dep in flow.get("dependency_order", []):
-                # print("222: ", flow_id, dep)
-                dep_str = str(dep)  # keep the keys as the same type
-                if dep_str in flows:
-                    graph[flow_id].append(dep_str)
-        visited = set()
-        order = []
-        for flow in graph:
-            if flow not in visited:
-                dfs(flow, visited, graph, order)
-        dependency_orders[group_key] = order[::-1]
-
-    return dependency_orders
-
 def get_flow_info(info):
+    """
+    Summarize most of the flow information from the json file and store in a dict
+    """
     flow_idx = [key for key in info if key.isdigit()]
     flow_info = {}
     link_cap = {}
@@ -59,6 +26,61 @@ def get_flow_info(info):
         flow_info.get(flow)['data_size'] = info.get(flow)['total']/1024/1024/8
         flow_info.get(flow)['links'] = set(info.get(flow)['links'])
     return flow_info
+
+def dfs(current_flow, visited, graph, order):
+    if current_flow in visited:
+        return
+    visited.add(current_flow)
+    for depenency in graph[current_flow]:
+        dfs(depenency, visited, graph, order)
+    order.append(current_flow)
+    return order
+
+def get_dependency_order(flow_info):
+    """
+    Get each flow group's dependency order
+    output -> {(1,1): ['1'], (1,2): ['3','2'], (2,3): ['4']}
+    """
+    groups = defaultdict(dict)
+    for flow_id, flow in flow_info.items():
+        groups[(flow["collective"], flow["group"])][flow_id] = flow
+
+    dependency_orders = {}
+    # key: (collective, group) value: flows
+    for group_key, flows in groups.items(): # e.g. (k=1, n=2), {'1': {'collective': 1, 'group': 1, 'source': 1, 'dest': 10, 'data_size': 150.0, 'links': {1, 2, 3, 4}, 'dependency_order': []}})
+        # Construct graph
+        graph = {flow_id: [] for flow_id in flows}
+        for flow_id, flow in flows.items():
+            for dep in flow.get("dependency_order", []):
+                # print("222: ", flow_id, dep)
+                dep_str = str(dep)  # keep the keys as the same type
+                if dep_str in flows:
+                    graph[flow_id].append(dep_str) # ex_graph = {'2': ['4'], '3': ['5'], '4': ['3'], '5': []}
+        visited = set()
+        order = []
+        for flow in graph:
+            if flow not in visited:
+                dfs(flow, visited, graph, order)
+        dependency_orders[group_key] = order[::-1]
+
+    return dependency_orders
+
+def fid_to_order(dependency_orders):
+    """
+    input -> {(1,1): ['1'], (1,2): ['3','2'], (2,3): ['4']}
+    output -> {'1': 1, '2': 2, '3': 1, '4': 1}
+    """
+    dep = {}
+    for key, values in dependency_orders.items():
+        for flow in values:
+            dep.setdefault(flow, []).append(values.index(flow) + 1)
+    return dep
+
+            
+
+
+
+
 
 
 # visited = set()
